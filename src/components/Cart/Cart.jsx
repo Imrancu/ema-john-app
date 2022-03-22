8da47951714fbd8ba1c125d8606b1d4c9e9952c3
imrancu <mdimran3772@gmail.com>
2022-03-23 01:02:48 +0600

    updated shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  console.log(cart);
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total += product.price * product.quantity;
+    shipping += product.shipping;
+  }
+  const tax = (total * 10) / 100;
+  const gradTotal = total + shipping + tax;
+
   return (
-    <div>
+    <div className="cart">
       <h3>Order summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Grand Total: ${gradTotal}</h4>
     </div>
   );
 };
