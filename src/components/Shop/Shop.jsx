9f7e12c242f3195224fd6994bf5ba1d4f9817e05
imrancu <mdimran3772@gmail.com>
2022-03-31 10:19:04 +0600

    used router

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useProducts from "../../hooks/useProducts";
 import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
-
-  useEffect(() => {
-    fetch("products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
 
   useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    for (const id in storedCart) {
-      const addedProduct = products.find((product) => product.id === id);
-      if (addedProduct) {
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        savedCart.push(addedProduct);
-      }
+    for(const id in storedCart){
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
     }
     setCart(savedCart);
-  }, [products]);
+},[products]);
 
   const handleAddToCart = (selectedProduct) => {
     let newCart = [];
@@ -56,7 +52,11 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart={cart} />
+        <Cart cart={cart}>
+          <Link to='/orders'>
+            <button>Review Order</button>
+          </Link>
+        </Cart>
       </div>
     </div>
   );
