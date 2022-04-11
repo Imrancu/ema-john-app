d62c3716807d436432d1973c5501a1c5ccd7550b
imrancu <mdimran3772@gmail.com>
2022-04-12 01:55:44 +0600

    update form validation

--- src/__auth_steps.jsx
+++ src/__auth_steps.jsx
@@ -1,8 +1,12 @@
 /**
- *1. Create a new project console.google.com 
- *2. npm install firebase
- *3. Create firebase.init.js and import getAuth and export default app
- *4.
+ * 1. Create a new firebase project in console.google.com 
+ * 2. npm install firebase
+ * 3. Create firebase.init.js and import getAuth and export default app
+ * 4. Firebase settings > Authentication > enable email and password auth
+ * 5. Create Login, Signup Component, Setup Route
+ * 6. attach form field handler and form submit handler
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
  * **/
 
 /**
